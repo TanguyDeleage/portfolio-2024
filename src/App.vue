f4d6707b071a760f52387c478b92fc0e187f8092
Tanguy Deleage <tanguy.deleage0@gmail.com>
2024-01-02 18:48:11 +0100

    Repo initialization + Responsive Hero

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+// import Header from './components/Header.vue'
+import Hero from './components/Hero.vue'
+</script>
+
+<template>
+
+  <header>
+    <Header />
+  </header>
+
+  <main>
+    <Hero />
+  </main>
+
+  <footer>
+  </footer>
+
+</template>
+
+<style scoped>
+
+@media (min-width: 1024px) {
+
+}
+</style>
