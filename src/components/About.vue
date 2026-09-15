eebf0eb12894c9acf0c36992bdf61ebd7ddd0578
Tanguy Deleage <tanguy.deleage0@gmail.com>
2026-09-15 16:28:07 +0200

    About description updated

--- src/components/About.vue
+++ src/components/About.vue
@@ -5,7 +5,8 @@
       <div class="div2 box"><div class="avatar"></div></div>
       <div class="div3 box">
         <p class="box-title">I'm Tanguy</p>
-        <p>Product Designer, graduated with a master's in management at Audencia and achieved a web developer bootcamp at LeWagon. My combined passions for entrepreneurship and tech brought me to this position where I can work on problems from their identification to their resolution.</p>
+        <p>I came to Product Design through a business degree and a full-stack bootcamp. This combination gives me both a business and technical perspective when designing complex B2B products.</p>
+        <p>I believe good design emerges from collaboration so I like bringing people into the process early, making ideas tangible a scalable.</p>
       </div>
       <div class="div4 box icon-container">
         <iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/1h9jfUmHvGBY8bT1IxP2iC?utm_source=generator" width="100%" height="152" frameBorder="0" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" title="Spotify playlist" loading="lazy"></iframe>
